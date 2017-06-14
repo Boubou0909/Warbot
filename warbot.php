bff28a4d9f987227b084924ae767b93762605fa2
Balthazar <balthazar0909@gmail.com>
2017-06-14 14:42:51 +0200

    Reglages_bugs+Améliorations

--- warbot.php
+++ warbot.php
@@ -18,28 +18,32 @@ $nom = '';
 
 $fichier = fopen('nb.txt', 'r+');
 fseek($fichier, 0);
-$var = intval(fgetc($fichier));
+$var = intval(fgets($fichier));
 fclose($fichier);
 
+$var = 4;
+
 $botman = BotManFactory::createForRTM([
-   'slack_token' => 'xoxb-197653180870-wQ0e9fB26btgngTTQN4K3P6q'
+   'slack_token' => 'xoxb-197653180870-OuMUI3e8w4KaNHBWGvtLpJML'
 ], $loop);
 
-$botman->hears('Warhammer begin',function($bot) use (&$var, &$etat, &$compteur, &$ligne, &$number, &$fichier, &$compteur){
+$botman->hears('Warbot begin',function($bot) use (&$var, &$etat, &$compteur, &$ligne, &$number, &$fichier, &$compteur){
    $bot->reply('Début du quiz :');
    $fichier = fopen('figurines.txt', 'r+');
    rewind($fichier);
    fseek($fichier, 0);
    $etat = 2;
-   $number = rand(0,$var);
-   for($i = 0; $i <= $number; $i++){
+   $number = rand(0,$var-1);
+   for($i = 0; $i < $number; $i++){
        $ligne = fgets($fichier);
        $ligne = fgets($fichier);
    };
    $ligne = fgets($fichier);
    $compteur = $compteur + 1;
    $bot->reply('Question n°' . $compteur);
+   $bot->typesAndWaits(1);
    $bot->reply($ligne);
+   $bot->typesAndWaits(1);
    $bot->reply('Qui est-ce ?');
    $bot->reply('(Pour répondre tapez : "Cette figurine est ...".)');
    $ligne = fgets($fichier);
@@ -51,36 +55,49 @@ $botman->hears('Cette figurine est {answer}',function($bot, $answer) use (&$var,
     $ligne = trim($ligne);
     if($ligne === $answer){
         $points = $points + 1;
+        $bot->typesAndWaits(1);
         $bot->reply('Bonne réponse !');
+        $bot->typesAndWaits(1);
         $bot->reply('Ton score est de : ' . $points);
     }
     else if($etat == 2 and $answer == 'End'){
         $etat = 0;
+        $bot->reply("Fin du jeu !");
+        $bot->typesAndWaits(1);
         $bot->reply("Ton score est de " . $points . ' sur ' . $compteur);
+        $compteur = 0;
+        $points = 0;
     }
     else if ($etat != 2){
-        $bot->reply("Pour commencer une partie, tapez : \"Warhammer begin\".");
+        $bot->typesAndWaits(1);
+        $bot->reply("Pour commencer une partie, tapez : \"Warbot begin\".");
     }
     else if ($etat == 2 and $answer != $ligne){
+        $bot->typesAndWaits(1);
         $bot->reply("Ce n'est pas la bonne réponse.");
+        $bot->typesAndWaits(1);
         $bot->reply('La bonne réponse était : ' . $ligne);
     }
+    $bot->typesAndWaits(1);
     $bot->reply('Taper "Ok" pour continuer et "End" pour finir.');
 });
 
 $botman->hears("{continue}",function($bot, $continue) use(&$var, &$etat, &$points, &$ligne, &$number, &$compteur) {
     if ($continue == "Ok" and $etat == 2) {
-        $fichier = fopen('figurines.txt', 'r');
+        $fichier = fopen('figurines.txt', 'r+');
         rewind($fichier);
-        $number = rand(0,$var);
-        for ($i = 0; $i <= $number; $i++) {
+        fseek($fichier, 0);
+        $number = rand(0,$var-1);
+        for($i = 0; $i < $number; $i++){
             $ligne = fgets($fichier);
             $ligne = fgets($fichier);
         };
         $ligne = fgets($fichier);
         $compteur = $compteur + 1;
         $bot->reply('Question n°' . $compteur);
+        $bot->typesAndWaits(1);
         $bot->reply($ligne);
+        $bot->typesAndWaits(1);
         $bot->reply('Qui est-ce ?');
         $bot->reply('(Pour répondre tapez : "Cette figurine est ...".)');
         $ligne = fgets($fichier);
@@ -89,10 +106,13 @@ $botman->hears("{continue}",function($bot, $continue) use(&$var, &$etat, &$point
     else if($continue == "End" and $etat == 2){
         $etat = 0;
         $bot->reply("Fin du jeu !");
+        $bot->typesAndWaits(1);
         $bot->reply("Ton score est de " . $points . ' sur ' . $compteur);
+        $compteur = 0;
+        $points = 0;
     }
     else if($etat == 0 and $continue != "Ajout d'une figurine"){
-        $bot->reply('Pour commencer une nouvelle partie, tapez "Warhammer begin".');
+        $bot->reply('Pour commencer une nouvelle partie, tapez "Warbot begin".');
     };
 });
 
@@ -109,8 +129,7 @@ $botman->hears("Ajout d'une figurine", function($bot) use (&$etat){
 $botman->hears("URL : {url}", function($bot, $url) use (&$etat, &$nom, &$http){
     if ($etat == 3) {
         $http = trim($url);
-        $bot->reply($http);
-        if (preg_match('#^https://#',$http) and preg_match('#jpg*|jpeg*|png*#', $http)) {
+        if (preg_match('#http(s)?://#',$http) and preg_match('#.jpg|.jpeg|.png#', $http)) {
             $bot->reply('Tapez maintenant le nom (en un seul mot) de la figurine, les majuscules étant importantes, comme ceci : "Nom : (Nom de la figurine)".');
         }
         else{
@@ -128,9 +147,9 @@ $botman->hears("Nom : {name}", function($bot, $name) use (&$etat, &$nom, &$http,
         $nom = trim($name);
         if ($http != '' and $nom != ""){
             $fichier = fopen('figurines.txt', 'a+');
-            fputs($fichier, \n);
+            fputs($fichier, "\r\n");
             fputs($fichier, $http);
-            fputs($fichier,\n);
+            fputs($fichier,"\r\n");
             fputs($fichier, $nom);
             fclose($fichier);
             $fichier = fopen('nb.txt', 'r+');
